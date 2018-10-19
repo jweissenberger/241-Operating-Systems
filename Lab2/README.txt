754ff307ffdbd9367ef3cd37bf588ef5b3e9ad5b
John Weissenberger <johnaweissenberger@gmail.com>
2018-10-19 08:15:10 -0400

    lab 2

---
+++ Lab2/README.txt
@@ -0,0 +1,11 @@
+NAME: Jack Weissenberger
+CSC-241 - Lab 2
+
+This project creates a fork that creates a child process. Both the child and parent 
+process read each line from the file and display it to the screen along with their 
+process ID number.
+
+Compile and execution instructions:
+Make sure that the path of the file you want to read is correct. It will varry from
+machine to machine. Other than that there is no command-line arguments, or special 
+compile instructions.
--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -1,3 +1,6 @@
+//Jack Weissenberger
+//241 Lab 2
+
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
@@ -5,14 +8,15 @@
 #include <errno.h>
 #include <string.h>
 
-
-
 int main() {
-    //open a text file named lab2.txt (be sure to include error handling)
+
     FILE *pf;
-    int errnum;
+    //location of file to read
     pf = fopen ("/home/csuser/Lab2/lab2.txt", "r");
 
+    int errnum;
+
+    // error handeling for reading a file
     if(pf == NULL) {
         errnum = errno;
         fprintf(stderr, "Value of errno: %d\n", errno);
@@ -20,29 +24,25 @@ int main() {
         fprintf(stderr, "Error opening file: %s\n", strerror(errnum));
     }
 
-    //create a process that will read and display each line of the file until the EOF is reached
-    // and then exits
     int output;
-    char *line ; //getline allocates space
-    unsigned long num ; // number of chars from getline
-    // in the line.
-    unsigned int r ; // Saves the return value given by getline().
-    line = NULL;
+    char *file_line; //getline allocates space
+    file_line = NULL;
+    unsigned long num; // number of chars from getline
     num= 0;
 
     pid_t pid = fork();
-    // s
+    // child process case
     if (pid == 0) {
-      while ((output = getline(&line, &num, pf)) != -1){
-        printf("Process: %d, Line: %s",getpid() ,line);
+      while ((output = getline(&file_line, &num, pf)) != -1){
+        printf("Process: %d, Line: %s",getpid() ,file_line);
       }
       exit(1);
 
     }
     //parent process case
     else {
-      while ((output = getline(&line, &num, pf)) != -1){
-        printf("Process: %d, Line: %s",getpid() ,line);
+      while ((output = getline(&file_line, &num, pf)) != -1){
+        printf("Process: %d, Line: %s",getpid() ,file_line);
         }
         exit(1);
       }
