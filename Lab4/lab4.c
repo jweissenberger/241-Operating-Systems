874eb4d076a71036be55acc966f745a28ef53321
John Weissenberger <johnaweissenberger@gmail.com>
2018-11-01 16:30:05 -0400

    Most of lab4

---
+++ Lab4/lab4.c
@@ -0,0 +1,108 @@
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
+#include "ezipc.h"
+
+
+int num_chairs, max_customers, cutCount=0, customerCount=0, randTime;
+int chair_sem, barber, m, cash;
+int* room;
+
+void customer(int id);
+void quitHandler(int);
+
+
+int main(int argc, char *argv[]) {
+    pid_t pid;
+
+    // set up the quitHandler
+    signal(SIGINT, quitHandler);
+
+    // set up everything for the semaphores and shared memory
+    SETUP();
+
+    if(argc!=3){
+        printf("ERROR");
+        exit(-1);
+    }
+
+    else{
+        printf("Barber Process id: %d\nKill barber process with: kill -USR1 %d\n",getpid(),getpid());
+        printf("Num Chairs: %s\nMax-Customers:%s\n",argv[1], argv[2]);
+
+        num_chairs = strtol(argv[1],NULL,0);
+        max_customers = strtol(argv[2],NULL,0);
+
+        chair_sem = SEMAPHORE(SEM_CNT, num_chairs);
+        barber = SEMAPHORE(SEM_BIN, 1);
+        room = SHARED_MEMORY(20);
+        cash = SEMAPHORE(SEM_BIN, 1);
+        m = SEMAPHORE(SEM_BIN, 1);
+        *room=num_chairs;
+
+
+        printf("%d\n", num_chairs);
+    }
+
+    srand(time(NULL));
+    randTime=rand()%5;
+    for(int i=1;i<=max_customers;i++){
+        pid=fork();
+        sleep(randTime);
+        if(pid==0){
+            printf("I am customer %d\n",i);
+            customer(i);
+            return 0;
+        }
+
+    }
+
+    //this is the barber sleeping
+    while(1){
+      P(barber); // this requests the barber, waking him up
+      P(m); // for the mutual exclusion, so that only you specifially are using
+      // the barber
+      sleep(rand()%10);
+      V(m);
+
+    }
+
+    return 0;
+}
+
+void customer(int id){
+    P(m);
+    if(*room <= 0){
+        printf("Customer %d Left because no chairs were available\n", id);
+    }
+    else{
+        printf("Customer %d Arrived, %d chairs left\n",id, --*room);
+    }
+    V(m);
+
+    P(barber);
+    P(m);
+    printf("Barber awakened, there is/are %d seats available\n",++*room);
+    V(m);
+    printf("Haircut: Customer %d is getting a haircut\n", id);
+    sleep(rand()%5);
+    V(barber);
+
+    // go to the cashier
+    P(cash);
+    printf("Customer %d is paying at the cashier\n", id);
+    sleep(rand()%5);
+    V(cash);
+    printf("Customer %d has paid the cashier\n", id);
+}
+
+void quitHandler ( int theInt) {
+  printf("\n DONE!\n");
+  printf("%d customers got their hair cut\n", max_customers);
+  exit(3);
+}
---
+++ Lab4/old_lab4.c
@@ -0,0 +1,109 @@
+// Jack Weissenberger Lab 4
+/*
+#include "ezipc.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int room, chairs, barber, m, cash, chair_ptr;
+char *available_seats;
+void quitHandler(int);
+void customer(int id);
+
+int main(){
+
+  SETUP(); // sets up the semaphores
+
+  signal ( SIGINT, quitHandler );
+
+  available_seats = SHARED_MEMORY(2);
+
+  int customers;
+  int finish;
+  // ask for number of chairs
+  printf("Please enter the number of chairs\n");
+  scanf("%d", &chairs);
+  // ask for number of customers
+  printf("Please enter the number of customers\n");
+  scanf("%d", &customers);
+
+  printf("Number of chairs: %d, Number of customers: %d\n", chairs, customers);
+
+  chair_ptr = *available_seats;
+  *available_seats = chairs;
+
+  room = SEMAPHORE(SEM_CNT, chairs);
+  cash = SEMAPHORE(SEM_BIN, 1);
+  barber = SEMAPHORE(SEM_BIN, 0);
+  m = SEMAPHORE(SEM_BIN, 0); // this is for the mutual exclusion
+
+  pid_t barber_pid = getpid();
+  printf("Barber process ID: %d\n", barber_pid);
+  printf("here\n");
+  for (int i = 0; i < customers; i++){
+
+    pid_t pid = fork();
+    sleep(rand()%10);
+
+    if (pid == 0){
+      customer(i);
+    }
+
+  }
+
+
+  //this is the barber sleeping
+  while(1){
+    P(barber); // this requests the barber, waking him up
+    P(m); // for the mutual exclusion, so that only you specifially are using
+    // the barber
+    sleep(rand()%10);
+    V(m);
+    V(room);
+
+    // need to create a control z handler to exit, look at the poject, you also
+    // need to print something out when you type control z
+  }
+
+  while(wait(&finish) != 1); // wait for all of the child processes
+
+  return 0;
+}
+
+
+void customer(int id){
+
+  printf("In customer\n" );
+  printf("available_seats: %d\n", (int)available_seats[0] );
+  if (available_seats > 0){
+    printf("there is a seat available\n" );
+  }
+
+  //request the room
+  P(room);
+  P(m);
+
+  available_seats[0] = (int)available_seats[0] - 1;
+  printf("Customer %d arrived, %d many seats remaining %d \n", id, (int)available_seats[0] );
+
+  //update everything
+
+  V(m);
+
+  //now wake up the barber, convert him to 0 from negative one
+  V(barber);
+
+  printf("Getting a haircut\n" );
+  // wait random time for while you are getting the haircut
+
+  P(cash);
+  // wait a random time at the cashier
+  V(cash);
+  exit(0);
+}
+
+void quitHandler ( int theInt) {
+  printf("\n Quitting!\n");
+  exit(3);
+}
+*/
